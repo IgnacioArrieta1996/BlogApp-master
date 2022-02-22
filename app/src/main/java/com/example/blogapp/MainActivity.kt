c7d92a4b40685e1ba69ab1380dad131eaad8bba0
IgnacioArrieta1996 <IgnacioArrieta.soft@gmail.com>
2022-02-22 07:01:25 -0300

    Práctica blogApp

---
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.blogapp
+
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.blogapp.core.hide
+import com.example.blogapp.core.show
+import com.example.blogapp.databinding.ActivityMainBinding
+import com.example.blogapp.databinding.FragmentHomeScreenBinding
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.lang.RuntimeException
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        observeDestinationChange()
+    }
+
+    private fun observeDestinationChange() {
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when(destination.id) {
+                R.id.loginFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.registerFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                R.id.setupProfileFragment -> {
+                    binding.bottomNavigationView.hide()
+                }
+
+                else -> {
+                    binding.bottomNavigationView.show()
+                }
+            }
+        }
+    }
+}
